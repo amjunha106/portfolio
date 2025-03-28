ddbe666b178d45a1967530ab8e2bd89cc9cdc9c7
amjunha106 <amjunha106@gmail.com>
2025-03-29 04:46:38 +0900

    1

---
+++ src/main/java/com/web/portfolio/Controller/HomeController.java
@@ -0,0 +1,33 @@
+package com.web.portfolio.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    // http://localhost:8080/Home
+    @GetMapping("/Home")
+    public String home(){
+        return "Home";
+    }
+
+    // http://localhost:8080/AboutMe
+    @GetMapping("/AboutMe")
+    public String AboutMe(){
+        return "AboutMe";
+    }
+
+    // http://localhost:8080/Portfolio
+    @GetMapping("/Portfolio")
+    public String Portfolio(){
+        return "Portfolio";
+    }
+
+    // http://localhost:8080/Contact
+    @GetMapping("/Contact")
+    public String Contact(){
+        return "Contact";
+    }
+
+}
